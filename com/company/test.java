d0f1d6985e92028701bf3220eb6b881714ea18ef
banjingking <avy192651@gmail.com>
2021-06-15 16:12:06 -0700

    add second

--- com/company/test.java
+++ com/company/test.java
@@ -11,4 +11,9 @@ public class test {
     public static void main(String[] args) {
         System.out.println("first");
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
